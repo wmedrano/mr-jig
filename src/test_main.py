f7c4aa968aa4b8ff579ecaeb1161c840f131ca7f
wmedrano <will.s.medrano@gmail.com>
2023-02-20 20:19:27 -0800

    Create flask server.

--- src/test_main.py
+++ src/test_main.py
@@ -1,2 +1,21 @@
-def test_example():
-    assert 2 + 2 == 4
+import pytest
+import main
+from flask.testing import Flask, FlaskClient
+
+
+@pytest.fixture()
+def app() -> Flask:
+    app = main.app
+    app.config.update({'TESTING': True})
+    return app
+
+
+@pytest.fixture()
+def client(app: Flask) -> FlaskClient:
+    return app.test_client()
+
+
+def test_homepage(client: FlaskClient):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.data is not None
