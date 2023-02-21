d76fa2abd3080cc7e3edd62feca6182c090e7bb8
Will <wmedrano@users.noreply.github.com>
2023-02-20 20:34:55 -0800

    Implement server. (#2)

--- src/main.py
+++ src/main.py
@@ -1,2 +1,15 @@
+from flask import Flask
+import os
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
 if __name__ == '__main__':
-    print('Hello World!')
+    port = int(os.getenv('PORT', '21894'))
+    app.run(host='0.0.0.0', port=port)
--- src/test_main.py
+++ src/test_main.py
@@ -1,2 +1,20 @@
-def test_example():
-    assert 2 + 2 == 4
+import pytest
+import main
+from flask import Flask
+from flask.testing import FlaskClient
+
+
+@pytest.fixture()
+def app() -> Flask:
+    return main.app
+
+
+@pytest.fixture()
+def client(app: Flask) -> FlaskClient:
+    return app.test_client()
+
+
+def test_homepage(client: FlaskClient):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.data is not None
